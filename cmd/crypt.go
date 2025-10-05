4e5ff1e8c84af8ddf186e36dc17bdb8036e021cb
ForSourceCodeAnalysis <sourceCodeAnalysis@aliyun.com>
2025-10-05 15:28:52 +0800

    调整crypt命令,支持远程批量解密文件/夹名

--- cmd/crypt.go
+++ cmd/crypt.go
@@ -1,14 +1,21 @@
 package cmd
 
 import (
-	log "github.com/sirupsen/logrus"
-
+	"context"
+	"encoding/json"
 	"io"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
 	"strings"
+	"time"
 
+	dcrypt "github.com/OpenListTeam/OpenList/v4/drivers/crypt"
+	"github.com/OpenListTeam/OpenList/v4/internal/db"
+	"github.com/OpenListTeam/OpenList/v4/internal/fs"
+	mmm "github.com/OpenListTeam/OpenList/v4/internal/model"
+	"github.com/OpenListTeam/OpenList/v4/internal/op"
 	"github.com/spf13/cobra"
 
 	rcCrypt "github.com/rclone/rclone/backend/crypt"
@@ -17,12 +24,21 @@ import (
 )
 
 // encryption and decryption command format for Crypt driver
+// 加解密命令
+// 1. 此命令可以用加密驱动的方式加密或解密本地文件/文件夹
+// 2. 如果设置了--remote-names，则可以对网盘里面加密文件夹里面的文件/夹名进行加解密转换
+//		比如，挂载了网盘A，并挂载了加密存储C，映射了A中的文件夹D，文件名和文件夹名都设置成了加密
+//		后面觉得，在网盘A中直接浏览D中的文件很不方便，通过此命令可以直接将D中的文件名重命名为加密前的文件名而不用加密解密文件内容
+//		注意：这个参数会直接修改网盘里的文件名，如果设置了cmount参数，会取消加密存储C的“文件/名加密”配置
+//
 
 type options struct {
-	op  string //decrypt or encrypt
-	src string //source dir or file
-	dst string //out destination
+	op          string //decrypt or encrypt
+	src         string //source dir or file
+	dst         string //out destination
+	remoteNames bool   //指定此项，会直接修改（加密/解密）网盘里面的加密文件夹里面的文件/夹名，但不会修改文件内容（仍然是加密的）
 
+	cmount             string //加密驱动挂载名称，如果提供了此项，会忽略下面的配置项，直接从数据库读取配置
 	pwd                string //de/encrypt password
 	salt               string
 	filenameEncryption string //reference drivers\crypt\meta.go Addtion
@@ -32,6 +48,7 @@ type options struct {
 }
 
 var opt options
+var cipher *rcCrypt.Cipher
 
 // CryptCmd represents the crypt command
 var CryptCmd = &cobra.Command{
@@ -58,7 +75,9 @@ func init() {
 	CryptCmd.Flags().StringVarP(&opt.src, "src", "s", "", "src file or dir to encrypt/decrypt")
 	CryptCmd.Flags().StringVarP(&opt.dst, "dst", "d", "", "dst dir to output,if not set,output to src dir")
 	CryptCmd.Flags().StringVar(&opt.op, "op", "", "de or en which stands for decrypt or encrypt")
+	CryptCmd.Flags().BoolVar(&opt.remoteNames, "remote-names", false, "only rename names of remote files")
 
+	CryptCmd.Flags().StringVar(&opt.cmount, "cmount", "", "the name of mounted crypt in db")
 	CryptCmd.Flags().StringVar(&opt.pwd, "pwd", "", "password used to encrypt/decrypt,if not contain ___Obfuscated___ prefix,will be obfuscated before used")
 	CryptCmd.Flags().StringVar(&opt.salt, "salt", "", "salt used to encrypt/decrypt,if not contain ___Obfuscated___ prefix,will be obfuscated before used")
 	CryptCmd.Flags().StringVar(&opt.filenameEncryption, "filename-encrypt", "off", "filename encryption mode: off,standard,obfuscate")
@@ -74,10 +93,10 @@ func (o *options) validate() {
 	if o.op != "encrypt" && o.op != "decrypt" && o.op != "en" && o.op != "de" {
 		log.Fatal("op must be encrypt or decrypt")
 	}
-	if o.filenameEncryption != "off" && o.filenameEncryption != "standard" && o.filenameEncryption != "obfuscate" {
+	if o.cmount == "" && o.filenameEncryption != "off" && o.filenameEncryption != "standard" && o.filenameEncryption != "obfuscate" {
 		log.Fatal("filename_encryption must be off,standard,obfuscate")
 	}
-	if o.filenameEncode != "base64" && o.filenameEncode != "base32" && o.filenameEncode != "base32768" {
+	if o.cmount == "" && o.filenameEncode != "base64" && o.filenameEncode != "base32" && o.filenameEncode != "base32768" {
 		log.Fatal("filename_encode must be base64,base32,base32768")
 	}
 
@@ -85,32 +104,35 @@ func (o *options) validate() {
 
 func (o *options) cryptFileDir() {
 	src, _ := filepath.Abs(o.src)
-	log.Infof("src abs is %v", src)
+	log.Printf("src abs is %v", src)
+
+	if o.remoteNames {
+
+		Init()
+		if o.cmount != "" {
+			ms, err := db.GetStorageByMountPath(o.cmount)
+			if err != nil {
+				log.Fatalf("can't find mount path %v, err: %v\n", o.cmount, err)
+			}
+			var ad dcrypt.Addition
+			err = json.Unmarshal([]byte(ms.Addition), &ad)
+			if err != nil {
+				log.Fatalf("unmarshal crypt addition err:%v \n", err)
+			}
+			createCipher(&ad)
+		} else {
+			createCipher(nil)
+		}
+		cryptNames(o.src)
+		return
+	}
 
 	fileInfo, err := os.Stat(src)
 	if err != nil {
 		log.Fatalf("reading file/dir %v failed,err:%v", src, err)
 
 	}
-	pwd := updateObfusParm(o.pwd)
-	salt := updateObfusParm(o.salt)
-
-	//create cipher
-	config := configmap.Simple{
-		"password":                  pwd,
-		"password2":                 salt,
-		"filename_encryption":       o.filenameEncryption,
-		"directory_name_encryption": o.dirnameEncryption,
-		"filename_encoding":         o.filenameEncode,
-		"suffix":                    o.suffix,
-		"pass_bad_blocks":           "",
-	}
-	log.Infof("config:%v", config)
-	cipher, err := rcCrypt.NewCipher(config)
-	if err != nil {
-		log.Fatalf("create cipher failed,err:%v", err)
-
-	}
+	createCipher(nil)
 	dst := ""
 	//check and create dst dir
 	if o.dst != "" {
@@ -132,24 +154,23 @@ func (o *options) cryptFileDir() {
 		//if src is dir and not set dst dir ,create ${src}_crypt dir as dst dir
 		dst = path.Join(filepath.Dir(src), fileInfo.Name()+"_crypt")
 	}
-	log.Infof("dst : %v", dst)
+	log.Printf("dst : %v\n", dst)
 
 	dirnameMap := make(map[string]string)
 	pathSeparator := string(os.PathSeparator)
 
 	filepath.Walk(src, func(p string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Errorf("get file %v info failed, err:%v", p, err)
-			return err
+			log.Fatalf("get file %v info failed, err:%v\n", p, err)
 		}
 		if p == src {
 			return nil
 		}
-		log.Infof("current path %v", p)
+		log.Printf("current path %v\n", p)
 
 		// relative path
 		rp := strings.ReplaceAll(p, src, "")
-		log.Infof("relative path %v", rp)
+		log.Printf("relative path %v\n", rp)
 
 		rpds := strings.Split(rp, pathSeparator)
 
@@ -177,7 +198,7 @@ func (o *options) cryptFileDir() {
 						} else {
 							dnn, err := cipher.DecryptDirName(rpds[i])
 							if err != nil {
-								log.Fatalf("decrypt dir name %v failed,err:%v", rpds[i], err)
+								log.Fatalf("decrypt dir name %v failed,err:%v\n", rpds[i], err)
 							}
 							rpds[i] = dnn
 							dirnameMap[oname] = dnn
@@ -191,7 +212,7 @@ func (o *options) cryptFileDir() {
 				dd = path.Join(dst, rp)
 			}
 
-			log.Infof("create output dir %v", dd)
+			log.Printf("create output dir %v", dd)
 			checkCreateDir(dd)
 			return nil
 		}
@@ -211,7 +232,7 @@ func (o *options) cryptFileDir() {
 			fdd = path.Join(fdd, strings.Join(rpds[:len(rpds)-1], pathSeparator))
 		}
 
-		log.Infof("file output dir %v", fdd)
+		log.Printf("file output dir %v", fdd)
 		o.cryptFile(cipher, p, fdd)
 		return nil
 	})
@@ -237,7 +258,7 @@ func (o *options) cryptFile(cipher *rcCrypt.Cipher, src string, dst string) {
 		filename := fileInfo.Name()
 		if o.filenameEncryption != "off" {
 			filename = cipher.EncryptFileName(fileInfo.Name())
-			log.Infof("encrypt file name %v to %v", fileInfo.Name(), filename)
+			log.Printf("encrypt file name %v to %v\n", fileInfo.Name(), filename)
 		} else {
 			filename = fileInfo.Name() + o.suffix
 		}
@@ -254,7 +275,7 @@ func (o *options) cryptFile(cipher *rcCrypt.Cipher, src string, dst string) {
 			if err != nil {
 				log.Fatalf("decrypt file name %v failed,err:%v", src, err)
 			}
-			log.Infof("decrypt file name %v to %v, ", fileInfo.Name(), filename)
+			log.Printf("decrypt file name %v to %v \n", fileInfo.Name(), filename)
 		} else {
 			filename = strings.TrimSuffix(filename, o.suffix)
 		}
@@ -308,4 +329,102 @@ func updateObfusParm(str string) string {
 		str, _ = strings.CutPrefix(str, obfuscatedPrefix)
 	}
 	return str
+}
+
+func createCipher(ad *dcrypt.Addition) {
+	if ad != nil {
+		opt.pwd = ad.Password
+		opt.salt = ad.Salt
+		opt.filenameEncryption = ad.FileNameEnc
+		opt.dirnameEncryption = ad.DirNameEnc
+		opt.filenameEncode = ad.FileNameEncoding
+		opt.suffix = ad.EncryptedSuffix
+
+	}
+	pwd := updateObfusParm(opt.pwd)
+	salt := updateObfusParm(opt.salt)
+	config := configmap.Simple{
+		"password":                  pwd,
+		"password2":                 salt,
+		"filename_encryption":       opt.filenameEncryption,
+		"directory_name_encryption": opt.dirnameEncryption,
+		"filename_encoding":         opt.filenameEncode,
+		"suffix":                    opt.suffix,
+		"pass_bad_blocks":           "",
+	}
+
+	c, err := rcCrypt.NewCipher(config)
+	if err != nil {
+		log.Fatalf("create cipher err: %v", err)
+	}
+	cipher = c
+
+}
+
+func cryptNames(path string) {
+	log.Printf("deal path： %s\n", path)
+	objs, err := fs.List(context.TODO(), path, &fs.ListArgs{
+		Refresh:            true,
+		WithStorageDetails: false,
+	})
+	if err != nil {
+		log.Printf("get list err dir name err:%v\n", err)
+		return
+
+	}
+	log.Printf("get list success,path: %v\n", path)
+	var renameObjects []mmm.RenameObj
+
+	for _, obj := range objs {
+		isdir := obj.IsDir()
+		name := obj.GetName()
+		if isdir {
+			time.Sleep(time.Second) //防止请求频率过快被限制
+			cryptNames(filepath.Join(path, name))
+		}
+		var newname string
+		if opt.op == "en" { //加密
+			newname = strings.TrimSuffix(name, opt.suffix)
+			if isdir {
+				newname = cipher.EncryptDirName(newname)
+			} else {
+				newname = cipher.EncryptFileName(newname)
+			}
+
+		} else { //解密
+			if isdir {
+				tn, err := cipher.DecryptDirName(name)
+
+				if err != nil {
+					log.Fatalf("decrypt dir name %v err: %v", name, err)
+				}
+				newname = tn
+			} else {
+				tn, err := cipher.DecryptFileName(name)
+				if err != nil {
+					log.Fatalf("decrypt filename %v err: %v", name, err)
+				}
+				newname = tn + opt.suffix //文件名拼接后缀
+			}
+
+		}
+
+		renameObjects = append(renameObjects, mmm.RenameObj{
+			ID:      obj.GetID(),
+			SrcName: obj.GetName(),
+			NewName: newname,
+		})
+
+	}
+	if len(renameObjects) == 0 {
+		return
+	}
+	//执行批量重命名
+	storage, actualPath, err := op.GetStorageAndActualPath(path)
+	err = fs.BatchRename(context.Background(), storage, actualPath, renameObjects)
+	if err != nil {
+		log.Fatalf("batchrename error : %v\n", err)
+	}
+	time.Sleep(time.Second) //防止请求频率过快被限制
+
 }
